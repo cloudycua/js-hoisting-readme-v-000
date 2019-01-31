b35354d1f9b399351ca40a48103611a688492d91
Christine Cua <cloudycua@gmail.com>
2019-01-31 16:44:58 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -17,13 +17,14 @@ function crazy() {
 }
 
 function sayMyName() {
-  // fix the code in here:
   var name = "Cricky";
 
+  function sayMy() {
+    var name = "Kristin";
+    console.log(name);
+  }
+  
   sayMy();
 
-  function sayMy() {
-    console.log(name);
-    var name = "Kristin";
-  }
+  
 }
